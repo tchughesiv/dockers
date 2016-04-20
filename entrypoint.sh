dcc0e1d07cad4ab324b2771a917b5bc6dfdc72a8
Dmitry <failsild@gmail.com>
2016-04-20 23:59:16 +0300

    kill nmon process if connection fail

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,7 @@ while true; do
     fn="/opt/nmon/$(date +%y%m%d_%H%M).nmon"
     pd="$(/opt/nmon/nmon -F $fn -s $s -c $c -T -p)"
     { echo "nmon p:default e:`hostname` f:`hostname`_file.nmon"; tail -f $fn --pid=$pd; } | nc $atsdUrl $atsdPort
+    kill -9 $pd
+    sleep 10
 done
 
