8ef3c6d7322571120bee62aa678f3b06e98daab1
Sild <failsild@gmail.com>
2016-04-22 19:16:24 +0300

    use z:date +%Z instead of localtime mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ fi
 while true; do
     fn="/opt/nmon/$(date +%y%m%d_%H%M).nmon"
     pd="$(/opt/nmon/nmon -F $fn -s $s -c $c -T -p)"
-    { echo "nmon p:default e:`hostname` f:`hostname`_file.nmon"; tail -f $fn --pid=$pd; } | nc $atsdUrl $atsdPort
+    { echo "nmon p:default e:`hostname` z:`date +%Z` f:`hostname`_file.nmon"; tail -f $fn --pid=$pd; } | nc $atsdUrl $atsdPort
     if kill -0 $pd 2>/dev/null; then
         kill -9 $pd
     fi
