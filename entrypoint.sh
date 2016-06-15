71e406223aea27c1239e372235c157b6ec691107
Sild <failsild@gmail.com>
2016-06-15 16:00:08 +0300

    remove extra keygen

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,5 @@ echo $password | tee /opt/$ftpuser-password
 
 echo "$ftpuser:$password" | chpasswd
 
-echo "y" | ssh-keygen -q -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-echo "y" | ssh-keygen -q -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-echo "y" | ssh-keygen -q -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-echo "y" | ssh-keygen -q -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 /usr/sbin/sshd -D
 
