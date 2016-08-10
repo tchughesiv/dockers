9426f1d3bcb87d9b7f3a1b415228df167f2264fc
Sild <failsild@gmail.com>
2016-08-10 17:02:52 +0300

    remove unused files

--- bin/restart_common_containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd $(readlink -f $0 | xargs dirname)
-mkdir -p ../logs
-./restart_dmitry_container.sh >> ../logs/dmitry_restart.log &
-./restart_gregory_container.sh >> ../logs/gregory_restart.log &
-./restart_travis_container.sh >> ../logs/travis_restart.log &
-./restart_rustam_container.sh >> ../logs/rustam_restart.log &
-
