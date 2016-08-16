120cb132861fb1459e22b89aa2d8beb184e0754d
Sild <failsild@gmail.com>
2016-08-16 16:54:22 +0300

    exclude unnecessary tests

--- entrypoint-api-test.sh
+++ entrypoint-api-test.sh
@@ -6,5 +6,5 @@ cd /home/axibase
 git clone -b master https://github.com/axibase/atsd-api-test
 while ! curl localhost:8088 >/dev/null 2>&1; do echo "waiting to start ATSD server ..."; sleep 3; done
 cd atsd-api-test
-mvn clean test -Dmaven.test.failure.ignore=false -DserverName="localhost" -Dlogin=$axiname -Dpassword=$axipass 
+mvn '-Dtest=!PropertyDeleteTest#testEntityTagsExactFalse+testExtraKeyExactFalse+testExtraKeyPropNoKeyExactFalse+testFutureDateExactFalse+testKeyMissmatchExactFalse+testTypeEntityExactFalse' clean test -Dmaven.test.failure.ignore=false -DserverName="localhost" -Dlogin=$axiname -Dpassword=$axipass 
 
