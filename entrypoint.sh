a75f0a92c17c20158ba2e257272ca239b78af340
rmakulov <rustam.makulov@axibase.com>
2016-11-18 16:58:25 +0300

    added fqdn option

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,22 @@ while getopts ab:c-: arg; do
   case $arg in
     - )  LONG_OPTARG="${OPTARG#*=}"
          case $OPTARG in
-           atsd-url=?* )  sed -i s,atsd_url,$LONG_OPTARG,g /etc/collectd/collectd.conf; sed -i s,atsd_url,$LONG_OPTARG,g /lvs.conf;;
-           lvs )  cp /lvs.conf /etc/collectd/collectd.conf;;
-           * )         echo "Illegal option --$OPTARG" >&2; exit 2 ;;
+           atsd-url=?* )
+                sed -i s,atsd_url,$LONG_OPTARG,g /etc/collectd/collectd.conf;
+                sed -i s,atsd_url,$LONG_OPTARG,g /lvs.conf
+                ;;
+           fqdn=?* )
+                if [ "$LONG_OPTARG" == "false" ]; then
+                    echo 'FQDNLookup false' >> /etc/collectd/collectd.conf;
+                    echo 'FQDNLookup false' >> /lvs.conf
+                fi
+                ;;
+           lvs )
+                cp /lvs.conf /etc/collectd/collectd.conf
+                ;;
+           * )
+                echo "Illegal option --$OPTARG" >&2; exit 2
+                ;;
          esac ;;
     \? ) exit 2 ;;  # getopts already reported the illegal option
   esac
